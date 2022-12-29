78ee9c4fdf01b02a110390dc3a2cd11b7d3ee60e
Dongd <choise154@gmail.com>
2022-12-29 16:58:48 +0900

    티켓 생성자 추가

--- src/main/java/chapter01/ticket/Ticket.java
+++ src/main/java/chapter01/ticket/Ticket.java
@@ -4,6 +4,10 @@ package chapter01.ticket;
 public class Ticket {
     private Long fee;
 
+    public Ticket(Long fee) {
+        this.fee = fee;
+    }
+
     public Long getFee() {
         return fee;
     }
