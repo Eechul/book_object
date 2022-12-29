f39b10d006f5d6e36479c5cdadc701f11684714f
Dongd <choise154@gmail.com>
2022-12-29 16:59:10 +0900

    첫 예제 테스트 코드 작성

---
+++ src/test/java/chapter01/ticket/TicketAppTest.java
@@ -0,0 +1,18 @@
+package chapter01.ticket;
+
+import org.junit.Test;
+
+public class TicketAppTest {
+
+    @Test
+    public void 관람객이_소극장에_입장한다() {
+        Ticket ticket = new Ticket(1000L);
+        TicketOffice ticketOffice = new TicketOffice(10000L, ticket);
+        TicketSeller ticketSeller = new TicketSeller(ticketOffice);
+
+        Bag bag = new Bag(1000L);
+        Audience audience = new Audience(bag);
+        Theather theather = new Theather(ticketSeller);
+        theather.enter(audience);
+    }
+}
