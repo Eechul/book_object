2cde9d8c702fac0c5ee6c5e813e3e2dcf16e5030
Dongd <choise154@gmail.com>
2022-12-29 16:44:01 +0900

    판매원 클래스 추가

---
+++ src/main/java/chapter01/ticket/TicketSeller.java
@@ -0,0 +1,14 @@
+package chapter01.ticket;
+
+// 판매원
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
